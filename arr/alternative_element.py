816fc5f5683808ef62e879e3e0f224044e9f4142
JiyadAhammad <jiyadahammad99@gmail.com>
2026-04-12 23:50:45 +0530

    Start Array:- Print Alternative Element in an array

---
+++ arr/alternative_element.py
@@ -0,0 +1,17 @@
+"""
+Input: arr[] = [10, 20, 30, 40, 50]
+Output: 10 30 50
+Explanation: Print the first element (10), skip the second element (20), print the third element (30), skip the fourth element(40) and print the fifth element(50).
+
+Input: arr[] = [-5, 1, 4, 2, 12]
+Output: -5 4 12
+"""
+
+
+def alternative_element(arr: list):
+    final_list = []
+    for i, val in enumerate(arr):
+        if i % 2 == 0:
+            final_list.append(val)
+
+    print(final_list)
---
+++ main.py
@@ -0,0 +1,4 @@
+from arr.alternative_element import alternative_element
+
+
+alternative_element([-5, 1, 4, 2, 12])
