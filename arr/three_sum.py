2603a7fb0a0110b52f7317fae76dc589c1d06c1f
JiyadAhammad <jiyadahammad99@gmail.com>
2026-06-13 11:55:23 +0530

    15. 3Sum

--- arr/three_sum.py
+++ arr/three_sum.py
@@ -12,6 +12,7 @@ Notice that the solution set must not contain duplicate triplets.
 Example 1:
 
 Input: nums = [-1,0,1,2,-1,-4]
+eg : [-4,-1,-1,0,1,2]
 Output: [[-1,-1,2],[-1,0,1]]
 Explanation:
 nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
@@ -45,6 +46,63 @@ def three_sum_brute(nums: list[int]) -> list[list[int]]:
                     sort = sorted([nums[i], nums[j], nums[k]])
                     if sort not in result:
                         result.append(sort)
+    return result
+
+
+def three_sum_optimal(nums: list[int]) -> list[list[int]]:
+    n = len(nums)
+    result = []
+
+    nums.sort()
+    # print(nums)
+
+    for i in range(n - 2):
+        left, right = i + 1, n - 1
+        # print([nums[i], nums[left], nums[right]])
+        while left < right:
+            three_sum = nums[i] + nums[left] + nums[right]
+            # print([nums[i], nums[left], nums[right]])
+            if three_sum == 0:
+                sort = [nums[i], nums[left], nums[right]]
+                if sort not in result:
+                    result.append(sort)
+                right -= 1
+                left += 1
+            elif three_sum > 0:
+                right -= 1
+            else:
+                left += 1
+
+    return result
+
+
+def three_sum_best(nums: list[int]) -> list[list[int]]:
+    n = len(nums)
+    result = []
+
+    nums.sort()
+
+    for i in range(n - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left, right = i + 1, n - 1
+
+        while left < right:
+            three_sum = nums[i] + nums[left] + nums[right]
+
+            if three_sum == 0:
+                result.append([nums[i], nums[left], nums[right]])
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                right -= 1
+                left += 1
+            elif three_sum > 0:
+                right -= 1
+            else:
+                left += 1
     return result
 
 
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from arr.most_water_container import maxArea_app
 from arr.move_zeros import move_zeros_brute
 from arr.sort_colors import sortColors, sortColors_app2
-from arr.three_sum import three_sum_brute, threeSum_app
+from arr.three_sum import (
+    three_sum_best,
+    three_sum_brute,
+    three_sum_optimal,
+    threeSum_app,
+)
 from arr.trapping_rain_water import trap_app
 from arr.two_sum_11 import two_sum_brute, two_sum_two_pointer
 
@@ -22,5 +27,5 @@ from arr.two_sum_11 import two_sum_brute, two_sum_two_pointer
 
 # sortColors_app2([2, 0, 2, 1, 1, 0])
 
-ans = three_sum_brute([-1, 0, 1, 2, -1, -4])
+ans = three_sum_best([-1, 0, 1, 2, -1, -4])
 print(ans)
