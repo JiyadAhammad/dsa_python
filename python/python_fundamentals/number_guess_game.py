225c26469457d287ee6d37651bab5ec3c5cc1db9
JiyadAhammad <jiyadahammad99@gmail.com>
2026-08-05 22:38:00 +0530

    refactored

--- python/python_fundamentals/number_guess_game.py
+++ python/python_fundamentals/number_guess_game.py
@@ -10,14 +10,14 @@ target = random.randint(1, 25)
 
 while True:
     try:
-        guess = int(input("Enter your guess"))
+        guess = int(input("Enter your guess --> "))
     except:
-        print("Please enter a valid guess")
+        print("Please enter a valid guess ")
         continue
     if guess == target:
-        print("Awesome!, You guessed")
+        print("Awesome!🤩, You guessed ")
         break
     elif guess > target:
-        print("You guessed a greater number")
+        print("You guessed a greater number \n")
     else:
-        print("You guessed a lower number")
+        print("You guessed a lower number \n")
