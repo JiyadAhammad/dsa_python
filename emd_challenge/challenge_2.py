2004e1c7655789447d16148538c7f6f9c3ed47d5
JiyadAhammad <jiyadahammad99@gmail.com>
2026-08-11 23:40:49 +0530

    2996. Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ emd_challenge/challenge_2.py
@@ -0,0 +1,59 @@
+"""
+2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
+
+You are given a 0-indexed array of integers nums.
+
+A prefix nums[0..i] is sequential if, for all 1 <= j <= i,
+nums[j] = nums[j - 1] + 1. In particular,
+the prefix consisting only of nums[0] is sequential.
+
+Return the smallest integer x missing from nums such that
+x is greater than or equal to the sum of the longest sequential prefix.
+
+Example 1:
+Input: nums = [1,2,3,2,5]
+Output: 6
+Explanation: The longest sequential prefix of nums is [1,2,3]
+with a sum of 6. 6 is not in the array,
+therefore 6 is the smallest missing integer greater than or equal to
+the sum of the longest sequential prefix.
+
+Example 2:
+Input: nums = [3,4,5,1,12,14,13]
+Output: 15
+Explanation: The longest sequential prefix of nums is [3,4,5]
+with a sum of 12. 12, 13, and 14 belong to the array while 15 does not.
+Therefore 15 is the smallest missing integer greater than or
+equal to the sum of the longest sequential prefix.
+
+Constraints:
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+"""
+
+from typing import List
+
+
+## Python Solution
+# Time Complexity = O2(n), overall (n)
+# Space Complexity = O(n),
+def missingInteger(nums: List[int]) -> int:
+    n = len(nums)
+    prefix = nums
+
+    for i in range(1, n):
+        if nums[i] != nums[i - 1] + 1:
+            prefix = nums[:i]
+            break
+
+    total = sum(prefix)
+
+    if total in nums:
+        while True:
+            if total + 1 in nums:
+                total += 1
+            else:
+                return total + 1
+
+    else:
+        return total
