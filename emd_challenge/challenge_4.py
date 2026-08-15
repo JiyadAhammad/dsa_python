743b8d291defde47584946bb02eed1eb2b52c779
JiyadAhammad <jiyadahammad99@gmail.com>
2026-08-15 10:52:38 +0530

    15. 3Sum

---
+++ emd_challenge/challenge_4.py
@@ -0,0 +1,73 @@
+"""
+15. 3Sum
+
+Given an integer array nums, return all the triplets \
+    [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, 
+    and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets 
+does not matter.
+
+Example 2:
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+"""
+
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        n = len(nums)
+        result = []
+
+        nums.sort()
+
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            while left <= right:
+                three_sum = nums[i] + nums[left] + nums[right]
+
+                if three_sum == 0:
+                    unique = sorted([nums[i], nums[left], nums[right]])
+                    if unique not in result:
+                        result.append(unique)
+                    left += 1
+                    right -= 1
+                if three_sum > 0:
+                    right -= 1
+                else:
+                    left += 1
+        return result
+
+        ## =========== Bruteforce Solution ==============
+        # n = len(nums)
+        # result = []
+
+        # for i in range(n - 2):
+        #     for j in range(i + 1, n - 1):
+        #         for k in range(j + 1, n):
+        #             three_sum = nums[i] + nums[j] + nums[k]
+        #             if three_sum == 0:
+        #                 # sort = [nums[i], nums[j], nums[k]]
+        #                 sort = sorted([nums[i], nums[j], nums[k]])
+        #                 if sort not in result:
+        #                     result.append(sort)
+        # return result
+
+
+# ============================================================================
