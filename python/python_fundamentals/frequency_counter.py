2d010c6d6872d4072a5b6a3dd3206281098ccaf9
JiyadAhammad <jiyadahammad99@gmail.com>
2026-08-16 11:36:02 +0530

    Frequency counter

--- python/python_fundamentals/frequency_counter.py
+++ python/python_fundamentals/frequency_counter.py
@@ -5,7 +5,7 @@ Walk a string or list, tally occurrences of each item into a dict.
 
 while True:
     try:
-        word = input("Enter a String")
+        word = input("Enter a String -> ")
 
         if not word.strip(" "):
             print("Please enter a valid string")
@@ -17,5 +17,14 @@ while True:
 frequency_map = {}
 n = len(word)
 
-for i in range(n):
-    pass
+for char in word:
+    frequency_map[char] = frequency_map.get(char, 0) + 1
+
+# for i in range(n):
+#     if word[i] not in frequency_map:
+#         frequency_map[word[i]] = 1
+#     else:
+#         frequency_map[word[i]] = frequency_map[word[i]] + 1
+
+
+print(frequency_map)
